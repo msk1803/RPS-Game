32211eeb850668969b02912adbeadcb9b5c4401a
Shubham Kumar <shubhamkumar@Shubhams-MacBook-Air.local>
2024-09-09 22:05:10 +0530

    second

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const rockButton = document.getElementById('rock');
 const paperButton = document.getElementById('paper');
 const scissorsButton = document.getElementById('scissors');
 const resultDisplay = document.getElementById('result');
+var winCount = 0;
+var lossCount = 0;
+var drawCount = 0;
 
 rockButton.addEventListener('click', () => playRound('rock'));
 paperButton.addEventListener('click', () => playRound('paper'));
@@ -16,6 +19,7 @@ function playRound(playerChoice) {
     // Determine the winner and update the resultDisplay with the outcome
     if (playerChoice === computerChoice) {
         resultDisplay.textContent = 'It\'s a draw!';
+        drawCount++;
     } 
     else if (
         (playerChoice === 'rock' && computerChoice === 'scissors') ||
@@ -23,7 +27,13 @@ function playRound(playerChoice) {
         (playerChoice === 'scissors' && computerChoice === 'paper')
     ) {
         resultDisplay.textContent = 'You win!';
+        winCount++;
     } else {
         resultDisplay.textContent = 'Computer wins!';
+        lossCount++;
     }
+    console.log(winCount);
+    document.getElementById('win').innerHTML = "Win :- " + winCount;
+    document.getElementById('draw').innerHTML = "Draw :- " + drawCount;
+    document.getElementById('loss').innerHTML = "Loss :- " + lossCount;
   }
